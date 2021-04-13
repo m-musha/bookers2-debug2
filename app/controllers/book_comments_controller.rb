03644e626c3d5a650d1ba64aaa6860456e188c7f
m-musha <gnmknr@gmail.com>
2021-04-13 14:01:09 +0000

    コメントview作成

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -1,11 +1,12 @@
 class BookCommentsController < ApplicationController
-  
+	before_action :authenticate_user!
+
 	def create
 		@book = Book.find(params[:book_id])
-		@comment = BookComment.new(book_comment_params)
-		@comment.book_id = @book.id
-		@comment.user_id = current_user.id
-		if @comment.save
+		@book_comment = BookComment.new(book_comment_params)
+		@book_comment.book_id = @book.id
+		@book_comment.user_id = current_user.id
+		if @book_comment.save
   		redirect_to book_path(@book.id)
 		else
 		  render 'books/show'
@@ -14,13 +15,14 @@ class BookCommentsController < ApplicationController
 
   def destroy
   	@book = Book.find(params[:id])
-  	co
+  	book_comment = @book.book_comments.find(params[:id])
+		book_comment.destroy
+		redirect_to request.referer
   end
   
   private
   
-  def list_params
+  def book_comment_params
     params.require(:book_comment).permit(:comment)
   end
-  
 end
--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,7 +6,7 @@ class BooksController < ApplicationController
     @books = Book.all
     @newbook = Book.new
     @user = @book.user
-    @comment = BookComment.new
+    @bookcomment = BookComment.new
   end
 
   def index
