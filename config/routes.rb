7f123648dd6d1e7d17ae8148f58842f27723ee0a
m-musha <gnmknr@gmail.com>
2021-04-14 14:25:56 +0000

    favoritesモデル、コントローラ作成

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
   resources :users, only: [:edit, :update, :index, :show]
   resources :books, only: [:edit, :create, :index, :show, :update, :destroy] do
    resources :book_comments, only: [:create, :destroy]
+   resource :favorites, only: [:create, :destroy]
   end 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_10_070436) do
+ActiveRecord::Schema.define(version: 2021_04_14_142101) do
 
   create_table "book_comments", force: :cascade do |t|
     t.text "comment"
@@ -26,7 +26,15 @@ ActiveRecord::Schema.define(version: 2021_04_10_070436) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  e
+  end
+
+  create_table "favorites", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "book_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
