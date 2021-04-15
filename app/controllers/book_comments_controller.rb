e95ca8295a335d9ce974ad11b866cf311239a23b
m-musha <gnmknr@gmail.com>
2021-04-15 07:27:37 +0000

    いいねボタンのコントローラを編集

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -5,7 +5,7 @@ class BookCommentsController < ApplicationController
 		@book = Book.find(params[:book_id])
 		@book_comment = BookComment.new(book_comment_params)
 		@book_comment.book_id = @book.id
-		@book_comment.user_id = current_user.id
+		@book_comment.user_id = current_user.id　#current_userはログインしているユーザーを取得している
 		if @book_comment.save
   		redirect_to book_path(@book.id)
 		else
--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,18 @@
 class FavoritesController < ApplicationController
+  before_action :authenticate_user!
+  # ログインしているユーザーのみにアクセスを許可する
+  
   def create
+    @book = Book.find(params[:book_id])
+    favorite = @book.favorites.new(user_id: current_user.id)
+    favorite.save
+    redirect_to request.referer #遷移先のURLを取得してリダイレクト
   end
 
   def destroy
+    @book = Book.find(params[:book_id])
+    favorite = @book.favorites.find_by(user_id: current_user.id)
+    favorite.destroy
+    redirect_to request.referer
   end
 end
